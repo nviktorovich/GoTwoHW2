27386371ba6a8aed7b70fe5e2554c6bdae52b8fa
nmaslenko <nvmaslenko@gmail.com>
2021-11-04 00:29:33 +0500

    add functions and describes

--- numdata.go
+++ numdata.go
@@ -1,21 +1,20 @@
 package numdata
 
 // IsPrime function return true if number is prime.
-//func IsPrime(num int) bool {
-//	var flag bool
-//	if num == 1 {
-//		return true
-//	}
-//	for i := 2; i < num/2; i++ {
-//		if num%i == 0 {
-//			return flag
-//		}
-//	}
-//	return !flag
-//}
-//
-//// IsOdd function return true if number is odd.
-//func IsOdd(num int) bool {
-//	return num % 2 != 0
-//}
-.
+func IsPrime(num int) bool {
+	var flag bool
+	if num == 1 {
+		return true
+	}
+	for i := 2; i < num/2; i++ {
+		if num%i == 0 {
+			return flag
+		}
+	}
+	return !flag
+}
+
+// IsOdd function return true if number is odd.
+func IsOdd(num int) bool {
+	return num%2 != 0
+}
